a92ed3ba0240cb5a63d307f7ee482f9e3df30f93
JuyeonOdosn <wbsksodkqh@naver.com>
2025-02-19 16:48:14 +0900

    [feat] 추가 NPC들과의 대화

--- Assets/Scripts/DialogueUI.cs
+++ Assets/Scripts/DialogueUI.cs
@@ -1,12 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; // Text, Button 등 UI 컴포넌트를 사용
 
-[System.Serializable]
 public class DialogueUI : MonoBehaviour
 {
-    
+    // Panel 오브젝트를 드래그 앤 드롭으로 할당
+    public GameObject dialoguePanel;
+    // Text 컴포넌트 (TextMeshProUGUI를 사용할 경우 해당 네임스페이스로 교체)
+    public TextMeshProUGUI dialogueText;
+    // 버튼을 누르면 대사가 넘어가도록 연결할 수 있음
+    public Button nextButton;  
+
+    // 대사 배열 예시
+    public string[] dialogues;
+    private int currentDialogueIndex = 0;
+
+    void Start()
+    {
+        // 게임 시작 시 대화창을 숨깁니다.
+        dialoguePanel.SetActive(false);
+        Debug.Log("메서드 스타트");
+    }
+
+    // NPC와 상호작용 시 호출 (예: NPC 스크립트에서 호출)
+    public void ShowDialogue()
+    {
+        currentDialogueIndex = 0;
+        dialoguePanel.SetActive(true);
+        dialogueText.text = dialogues[currentDialogueIndex];
+    }
+
+    // "다음" 버튼 클릭 시 호출되는 메서드
+    public void OnNextButtonClicked()
+    {
+        currentDialogueIndex++;
+        if (currentDialogueIndex < dialogues.Length)
+        {
+            dialogueText.text = dialogues[currentDialogueIndex];
+        }
+        else
+        {
+            HideDialogue();
+        }
+    }
+
+    public void HideDialogue()
+    {
+        dialoguePanel.SetActive(false);
+    }
 }
-    
+
 
--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -2,28 +2,26 @@ using UnityEngine;
 
 public class NPCInteraction : MonoBehaviour
 {
-    public GameObject dialogueUI;
-
-    private void Start()
-    { 
-        dialogueUI.SetActive(false);
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            dialogueUI.SetActive(true);
-        }
-    }
+    // DialogueUI 스크립트가 붙은 오브젝트를 Inspector에서 연결합니다.
+    public DialogueUI dialogueUI;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("충돌인식");
+        // 플레이어 태그가 "Player"로 설정되어 있어야 합니다.
         if (other.CompareTag("Player"))
         {
-            dialogueUI.SetActive(true);
+            Debug.Log("플레이어 인식");
+            dialogueUI.ShowDialogue();
         }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("플레이어 인식");
+            dialogueUI.HideDialogue();
+        }
+    }
 }
-
